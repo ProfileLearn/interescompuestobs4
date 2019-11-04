db37dac1c81df137a70d40959b4a331a6937a460
profilelearn <profilelearn@gmail.com>
2019-11-04 19:37:15 -0300

    Interes compuesto BS4 Primer commit

---
+++ js/codigo.js
@@ -0,0 +1,53 @@
+'use strict';
+
+// Acceso al DOM
+
+let tna = document.querySelector('#tnaIn');
+let montoInicial = document.querySelector('#inicialIn');
+let plazo = document.querySelector('#plazoIn');
+let ciclos = document.querySelector('#renovIn');
+let resultado = document.querySelector('#resultadoIn');
+let btn = document.querySelector('#calcular');
+
+
+// Variables
+
+let tnaVal;
+let montoInicialVal;
+let plazoVal;										// ESTUDIAR BIEN AMBITO DE FUNCIONES, VARIABLES, RETORNOS
+let ciclosVal;										// FUNCIONES ANONIMAS Y DE FLECHA
+let resParcial;
+
+//funcion
+function saveValue()
+{
+	
+	tnaVal = tna.value;
+	montoInicialVal = montoInicial.value;
+	plazoVal = plazo.value;
+	if (ciclos.value < 1) {ciclos.value = 1};
+ 	ciclosVal = ciclos.value;
+	interes(montoInicialVal, tnaVal, plazoVal, --ciclosVal);
+	resultado.value = resParcial;
+
+}
+
+function interes(montoInicial, tna, plazo, ciclos) 
+{
+	let tnaPorDia = ((tna / 365) / 100);
+	resParcial = montoInicial * ((tnaPorDia * plazo) + 1);
+	console.log(resParcial + ' resultado parcial');
+	console.log(ciclos + ' ciclos antes de --');
+
+	if (ciclos > 0)
+	{
+		//ciclos--;
+		console.log(ciclos + ' ciclos desp --');
+		interes(resParcial, tna, plazo, --ciclos);
+	}
+	else {
+		return resParcial;
+	}		
+};
+
+btn.addEventListener ('click', function(){saveValue();});
