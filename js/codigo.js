cfc273a6360c47d32899041bbcc1c0f6beb39144
Profile Learn <profilelearn@gmail.com>
2021-02-06 09:17:32 -0300

    Agrega Adicional

--- js/codigo.js
+++ js/codigo.js
@@ -1,60 +1,40 @@
-'use strict';
 
-// Acceso al DOM
+const obj = {
+	tna: document.getElementById('tnaIn'),
+	montoInicial: document.getElementById('inicialIn'),
+	plazo: document.getElementById('plazoIn'),
+	ciclos: document.getElementById('renovIn'),
+	adicional: document.getElementById('adicionalIn'),
+	resultado: document.getElementById('resultadoIn')
+}
 
-let tna = document.querySelector('#tnaIn');
-let montoInicial = document.querySelector('#inicialIn');
-let plazo = document.querySelector('#plazoIn');
-let ciclos = document.querySelector('#renovIn');
-let resultado = document.querySelector('#resultadoIn');
-let btn = document.querySelector('#calcular');
-let btnClear = document.querySelector('#clear');
 
-// Variables
-
-let tnaVal;
-let montoInicialVal;
-let plazoVal;										// ESTUDIAR BIEN AMBITO DE FUNCIONES, VARIABLES, RETORNOS
-let ciclosVal;										// FUNCIONES ANONIMAS Y DE FLECHA
-let resParcial;
-
+function interes(montoInicial, tna, plazo, ciclos, adicional=0) 
+	{	
+		const tnaPorDia = ((tna / 365) / 100);
+		adicional *= 1;
+		let resParcial = (montoInicial) * ((tnaPorDia * plazo) + 1);
+		resParcial += adicional;
+		if (ciclos > 1) 
+		{	
+			return interes(resParcial, tna, plazo, --ciclos, adicional);
+		} 
+		return resParcial;
+	};
 //funcion
-function saveValue()
-{
-	
-	tnaVal = tna.value;
-	montoInicialVal = montoInicial.value;
-	plazoVal = plazo.value;
-	if (ciclos.value < 1) {ciclos.value = 1};
- 	ciclosVal = ciclos.value;
-	interes(montoInicialVal, tnaVal, plazoVal, --ciclosVal);
-	resultado.value = resParcial;
+
+function calHandleClick(e) {
+	let {montoInicial, tna, plazo, ciclos, resultado, adicional} = obj;
+	ciclos.value = ciclos.value < 1 ? 1 : ciclos.value;
+	resultado.value = interes(montoInicial.value, tna.value, plazo.value, ciclos.value, adicional.value);
 
 }
 
-function interes(montoInicial, tna, plazo, ciclos) 
-{
-	let tnaPorDia = ((tna / 365) / 100);
-	resParcial = montoInicial * ((tnaPorDia * plazo) + 1);
-	console.log(resParcial + ' resultado parcial');
-	console.log(ciclos + ' ciclos antes de --');
-
-	if (ciclos > 0)
-	{
-		//ciclos--;
-		console.log(ciclos + ' ciclos desp --');
-		interes(resParcial, tna, plazo, --ciclos);
+function clearHandleClick() {
+	for (prop in obj) {
+		obj[prop].value = "";
 	}
-	else {
-		return resParcial;
-	}		
-};
+}
 
-btn.addEventListener ('click', function(){saveValue()});
-btnClear.addEventListener('click', function(){
-	tna.value = '';
-	montoInicial.value = '';
-	plazo.value = '';
-	ciclos.value = '';
-	resultado.value = '';
-});
+document.querySelector('#calcular').addEventListener('click', calHandleClick);
+document.querySelector('#clear').addEventListener('click', clearHandleClick);
