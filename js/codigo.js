fe8e1e42baf6843b129c447d56b8064a65e4dc88
ProfileLearn <profilelearn@gmail.com>
2023-08-14 20:01:30 -0300

    agregando resultados netos

--- js/codigo.js
+++ js/codigo.js
@@ -7,26 +7,23 @@ QUITAR resultado de obj ok
 CONVERTIR ESOS NÚMEROS NUEVAMENTE A STRING PARA MOSTRAR EN EL INPUT DE RESULTADO ok
 Unificar las funciones dotseparation ok
 Revisar que los datos se conviertan a tipo number antes de llamar a funcion interes ok
-Agregar manejador de "," (comas en el formulario)
-Cuando se permita agregar un handler para que se pueda usar en el porcentaje
-y un handler para que si se presiona "." se convierta en ","
-La "," luego deberá a ser convertida en "." cuando se convierta string a number
-*/
-/*
-Handler input
--formatear texto
-handler click
-
-
+Agregar manejador de "," (comas en el formulario) ok
+Cuando se permita agregar un handler para que se pueda usar en el porcentaje 
+y un handler para que si se presiona "." se convierta en "," (ok, solo en convierte punto por coma en el porcentaje)
+La "," luego deberá a ser convertida en "." cuando se convierta string a number ok
 */
 
-const obj = {
+// Crea un objeto con referencias a los elementos del formulario
+
+const domObject = {
 	tna: document.getElementById('tnaIn'),
 	montoInicial: document.getElementById('inicialIn'),
 	plazo: document.getElementById('plazoIn'),
 	ciclos: document.getElementById('ciclosIn'),
 	adicional: document.getElementById('adicionalIn'),
 }
+
+// función que calcula el interés (ver la posibilidad de refactorizarla como función con recursión de cola)
 
 function interes({ montoInicial, tna, plazo, ciclos, adicional }) {
 	const tnaPorDia = ((tna / 365) / 100);
@@ -39,24 +36,71 @@ function interes({ montoInicial, tna, plazo, ciclos, adicional }) {
 	return resParcial;
 };
 
+// Se calcula el neto obtenido restando el monto inicial al resultado final
+
+
+function textToNumObject(stringObject) {
+	for (const prop in stringObject) {
+		stringObject[prop] = textToNum(stringObject[prop]);
+		if (isNaN(stringObject[prop])) { stringObject[prop] = 0 };
+	}
+	return stringObject;
+}
+
 function calHandleClick() {
 
-	const values = obtainValues(obj);
-	for (const prop in values) {
-		values[prop] = textToNum(values[prop]);
-		if (isNaN(values[prop])) { values[prop] = 0 };
-	}
+	// primero se obtiene las referencias de los campos necesitados (ver si no es más eficiente hacerlo despúes para no ocupar memoria)
 
-	let retorno = interes(values)
-	// hasta aquí retorno es number
-	retorno = retorno.toFixed(2); // aquí se convierte a strings
-	retorno = dotFormat(retorno);
+	// const inicialIn = document.getElementById('inicialIn');
+
+
+	// const resultadoCiclo = document.getElementById('resultadoCiclo');
+
+	// Se obtienen los valores de obj y se los convierte a números
+
+	const formValues = obtainDomValues(domObject); // OBJECT CON REFERENCIAS A ELEMENTOS DEL DOM
+	const formNumValues = textToNumObject(formValues); // OBJECT CON NÚMEROS
+	
+
+	const retornoResultado = interes(formNumValues)
+	// hasta aquí retornoResultado es number
+	let retornoResultadoString = retornoResultado.toFixed(2); // aquí se convierte a strings
+	retornoResultadoString = dotFormat(retornoResultadoString);
+
+	const montoInicial = formNumValues.montoInicial;
+	const tna = formNumValues.tna;
+	const plazo = formNumValues.plazo;
+	
+
+	const resultadoCicloNum = interes({ montoInicial: retornoResultado, tna: tna, plazo: plazo, ciclos: 1, adicional: 0 });
+	const resultadoCicloNumNet = resultadoCicloNum - retornoResultado;
+	let resultadoCicloNumStr = resultadoCicloNumNet.toFixed(2);
+	resultadoCicloNumStr = dotFormat(resultadoCicloNumStr);
+
+
+
+	const netResultNum = (retornoResultado - montoInicial);
+	let netResultStr = netResultNum.toFixed(2); 
+	netResultStr = dotFormat(netResultStr);
+	
+
+	// let retornoResultadoCiclo = interes(values)
+	// // hasta aquí retornoResultadoCiclo es number
+	// retornoResultadoCiclo = retornoResultadoCiclo.toFixed(2); // aquí se convierte a strings
+	// retornoResultadoCiclo = dotFormat(retorno);
+
 	const resultado = document.getElementById('resultadoIn');
-	render(resultado, retorno)
+	const resultadoNeto = document.getElementById('resultadoNeto');
+	const resultadoCiclo = document.getElementById('resultadoCiclo');
+
+
+	render(resultado, retornoResultadoString);
+	render(resultadoNeto, netResultStr);
+	render(resultadoCiclo, resultadoCicloNumStr);
 }
 
 
-function obtainValues(values) {
+function obtainDomValues(values) {
 	const objValues = {};
 	for (const prop in values) {
 		objValues[prop] = values[prop].value;
@@ -70,11 +114,15 @@ function clearHandleClick() {
 	}
 }
 
+// pasa texto a número y elimina los puntos
+
 function textToNum(text) {
 	const transitionText = text.replace(/\./g, '')
 	const number = Number.parseFloat(transitionText);
 	return number;
 }
+
+// formatea los números a texto con (.) como separador de miles
 
 function dotFormat(text, dinamic = false) {
 
