0911db360521105499f77bf14e1a0fb941c0934c
ProfileLearn <profilelearn@gmail.com>
2025-03-23 22:33:32 -0300

    events handlers end

--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,9 @@ No declarar tantas veces las mismas variables o referencias al DOM
 Refactorizar
 */
 
-import { interes, render, numAjustado, teayt, rendimiento } from './methods.js'
+import { interes, render, numAjustado, teayt, rendimiento } from './utils/calcMethods.js'
 import { dotFormat, textToNumObject } from './utils/parser.js'
-import { obtainDomValues, tooltipHandler, handleViewMore, clear, inputHandler } from './utils/events_handlers.js'
+import { obtainDomValues, tooltipHandler, handleViewMore, clear, inputHandler } from './utils/eventsHandlers.js'
 
 
 // Crea objetos con referencias a los elementos del formulario
@@ -33,19 +33,6 @@ const thrDomObject = {
 	resultadoCiclo: document.getElementById('resultadoCiclo')
 }
 
-// export const tooltipsObject = {
-// 	tna: 'Tasa Nominal Anual. Porcentaje de interés a 365 días, sin tocar la inversión',
-// 	inicial: 'Monto inicial a invertir',
-// 	plazo: 'Cantidad de días que dura el ciclo de inversión',
-// 	ciclos: 'Cantidad de ciclos en que se renueva la inversión',
-// 	adicional: 'Monto adicional que se agrega en cada ciclo',
-// 	ita: 'Inflación Total Acumulada al terminar el último ciclo (estimación)',
-// 	resultado: 'Monto total al finalizar el último ciclo',
-// 	resultadoNeto: 'Monto neto al finalizar el último ciclo (Resultado - Monto inicial)',
-// 	resultadoCiclo: 'Indica una proyección de cuál sería el Monto neto invirtiendo el Monto final (Resultado) después de 1 ciclo más',
-// 	tea: 'Tasa Efectiva Anual. Porcentaje de interés real basada en plazo y cantidad de ciclos proyectado a 365 días',
-// 	tet: 'Tasa Efectiva Total. Porcentaje de interés real al final de la inversión'
-// }
 
 const porcentajes = {};
 
---
+++ src/js/utils/calcMethods.js
@@ -0,0 +1,39 @@
+
+// función que calcula el interés (ver la posibilidad de refactorizarla como función con recursión de cola)
+
+export function interes({ montoInicial, tna, plazo, ciclos, adicional }) {
+  const tnaPorDia = ((tna / 365) / 100);
+  adicional *= 1;
+  let resParcial = (montoInicial) * ((tnaPorDia * plazo) + 1);
+  resParcial += adicional;
+  if (ciclos > 1) {
+    return interes({ montoInicial: resParcial, tna, plazo, ciclos: ciclos - 1, adicional });
+  }
+  return resParcial;
+};
+
+export function teayt(tna, plazo, ciclos) {
+  // fórmula = (((1+((tna÷100)÷(días_del_año / plazo)))^ciclos)−1)×100
+
+  const tea = 100 * (Math.pow(1 + ((tna / 100) / 12), 12) - 1)
+
+  const tet = 100 * (Math.pow(1 + ((tna / 100) / (365 / plazo)), ciclos) - 1)
+
+  return { tea, tet }
+}
+
+export function rendimiento(ita, tet) {
+  const porcentajeIta = ita / 100;
+
+  const porcentajeTet = tet / 100;
+
+  return (((1 + porcentajeTet) / (1 + porcentajeIta)) - 1) * 100
+}
+
+export function numAjustado(ita = 0, num) {
+  return num / (1 + (ita / 100));
+}
+
+export function render(element, value) {
+  element.value = value;
+}
